64b343eeea83570bf66362a583adf6b3fdc9471b
Jin huijia <Huijia.Jin@tcs.com>
2025-06-10 13:43:44 +0900

    second message

--- gitpractice.java
+++ gitpractice.java
@@ -1,4 +1,4 @@
-public class gitparctice {
+public class gitpractice {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello,hello");
